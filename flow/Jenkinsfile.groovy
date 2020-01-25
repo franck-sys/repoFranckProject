8e3c0574045c1515367ddc72629798b872bdcc29
franck-sys <57967195+franck-sys@users.noreply.github.com>
2020-01-25 14:06:14 +0100

    "jenkinsfile"

---
+++ flow/Jenkinsfile.groovy
@@ -0,0 +1,37 @@
+pipeline {
+node{
+   jdk - tool name:'jdk1.8'
+   maven - tool name:'maven3'
+   checkout scm
+
+}
+  agent any
+  stages {
+    stage('Unit Test') { 
+      steps {
+        sh 'mvn clean test'
+      }
+    }
+    
+    stage('Deploy CloudHub') { 
+      environment {
+        ANYPOINT_CREDENTIALS = credentials('anypoint.credentials')
+		ANYPOINT_BUSINESS_GROUP = "cloudhub organisation"
+		ANYPOINT_URI = "https://anypoint.mulesoft.com"
+		ANYPOINT_APPLICATION_NAME = "flow-training-example"
+		ANYPOINT_DEPLOYMENT_ENV_DEV = "developement"
+		ANYPOINT_DEPLOYMENT_DEV_NAME = "Training"
+		ANYPOINT_DEPLOYMENT_ENV_STAGING = "test"
+		ANYPOINT_DEPLOYMENT_STAGING_NAME ="test_repo_staging"
+		ANYPOINT_DEPLOYMENT_REGION ="eu-west-2"
+		ANYPOINT_DEPLOYMENT_WORKTYPE = "micro"
+		ANYPOINT_DEPLOYMENT_WORKS = "1"
+		MAVEN_HOME = ${maven3}
+		JAVA_HOME = ${jdk1}
+      }
+      steps {
+        sh 'mvn deploy -P cloudhub -Dmule.version=4.2.2 -Danypoint.username=${franckTeguia} -Danypoint.password=${Franck*2020}' 
+      }
+    }
+  }
+}
